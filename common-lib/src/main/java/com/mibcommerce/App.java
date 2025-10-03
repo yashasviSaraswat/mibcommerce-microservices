e4ff6282a5bddbc18deed70666ff999b4f06921e
yashasviSaraswat <saraswat2004yashasvi@gmail.com>
2025-10-03 12:07:03 +0530

    Add .gitignore to ignore build and IDE files

---
+++ common-lib/src/main/java/com/mibcommerce/App.java
@@ -0,0 +1,13 @@
+package com.mibcommerce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ common-lib/src/main/java/com/mibcommerce/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.mibcommerce.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String email;
+    private String name;
+    private String role;
+}
